46bb305bb5881241074fbf21e93782eee50421c1
Sardar <your-email@example.com>
2026-06-02 17:55:18 +0600

    ДЗ

---
+++ lesson3.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self,color,model):
+        self.model = model 
+        self.color = color
+        self._fined = False
+        self.__max_speed = 0
+
+    def _calculate_fuel_
+    
