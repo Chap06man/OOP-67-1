bafbae504a497519474ee08c50bda61a9d5e6b14
Sardar <your-email@example.com>
2026-06-22 01:50:38 +0600

    Получение Базы Данных

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sqlite3
 
 from lessons.database import (
     create_tables,
-    add_student
+    add_student,
+    get_students,
+    get_student_by_name,
+    get_student_by_id,
+    delete_student,
+    update_student,
 )
-
 
 if __name__ == "__main__":
     connection = sqlite3.connect(
@@ -20,14 +24,62 @@ if __name__ == "__main__":
     )
     add_student(
         connection,
-        'Sardar',
-        20,
-        'Batken'
-        )
+        'Aijamal',
+        23,
+        'Naryn'
+    )
     add_student(
         connection,
-        'Adema',
-        16,
+        'Igor',
+        25,
+        'Karakol'
+    )
+    add_student(
+        connection,
+        'Igor',
+        32,
         'Bishkek'
     )
+    print("== get students ==")
+    students = get_students(connection)
+    for st in students:
+        print(st)
+
+    print("== search by name ==")
+    student = get_student_by_name(
+        connection,
+        'Igor',
+    )
+    print(student)
+    student2 = get_student_by_name(
+        connection,
+        'Dastan',
+    )
+    print(student2)
+    student3 = get_student_by_id(
+        connection,
+        10,
+    )
+    print(student3)
+
+    print("== delete students ==")
+    delete_student(
+        connection,
+        4
+    )
+    print(get_students(connection))
+
+    print("== update students ==")
+    update_student(
+        connection,
+        'Artur',
+        20,
+        'Kara-Balta',
+        1
+    )
+    print(get_student_by_id(
+        connection,
+        1
+    ))
+
     connection.close()
